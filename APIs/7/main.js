6b9820095efc4a6dce07a7481c86146dd178f779
yunhosung <yhs4157@naver.com>
2021-12-07 17:09:13 +0900

    form 관련 input, first step

--- APIs/7/main.js
+++ APIs/7/main.js
@@ -4,3 +4,97 @@ const bodyInput = document.querySelector('#body');
 const form = document.querySelector('form'); 
 const submitBtn = document.querySelector('form button'); 
 
+let db; 
+
+window.onload = function() {
+    let request = window.indexedDB.open('notes_db', 1); 
+
+    request.onerror = function() {
+        console.log('Database failed to open'); 
+    };
+
+    request.onsuccess = function() {
+        console.log('Database opend successfully'); 
+        db = request.result;
+        displayData(); 
+    };
+
+    request.onupgradeneeded = function(e) {
+        let db = e.target.result; 
+
+        let objectStore = db.createObjectStore('notes_os', {keyPath: 'id', autoIncrement: true}); 
+
+        objectStore.createIndex('title', 'title',  {unique: false});
+        objectStore.createIndex('body', 'body', {unique: false}); 
+
+        console.log('Database setup complete'); 
+    };
+
+    form.onsubmite = addData; 
+
+    function addData(e) {
+        e.preventDefault(); 
+
+        let newItem = {title: titleInput.value, body: bodyInput.value}; 
+        let transaction = db.transaction(['notes_os'], 'reqdwrite');
+        let objectStore = transaction.objectSotre('notes_os'); 
+
+        var request = objectStore.add(newItem); 
+        request.onsuccess = function() {
+            titleInput.value = ''; 
+            bodyInput.value = ''; 
+        };
+        
+        transaction.oncomplete = function() {
+            console.log('Transaction completed: database modification finished.'); 
+            displayData(); 
+        };
+
+        transaction.onerror = function() {
+            console.log('Transaction not opened due to error'); 
+        };
+    }
+
+    function displayData() {
+        while(list.firstChild) {
+            list.removeChild(list.firstChild); 
+        }
+
+        let objectStore = db.transaction('notes_os').objectStore('notes_os'); 
+        objectStore.openCursor().onsuccess = function(e) {
+            let cursor = e.target.result; 
+
+            if(cursor) {
+                const listItem = document.createElemnet('li'); 
+                const h3 = document.createElement('h3'); 
+                const para = document.createElement('p'); 
+
+                listItem.appendChild(h3); 
+                listItem.appendChild(para); 
+                list.appendChild(listItem); 
+
+                h3.textContent = cursor.value.title; 
+                para.textContent = cursor.value.body; 
+
+                listItem.setAttribute('data-note-id', cursor.value.id); 
+
+                const deleteBtn = document.createElement('button'); 
+                listItem.appendChild(deleteBtn); 
+                deleteBtn.textContent = 'Delete';
+
+                deleteBtn.onclick = deleteItem; 
+
+                cursor.continue(); 
+            } else {
+                if(!list.firstChild) {
+                    const listItem = document.createElement('li'); 
+                    listItem.textContent = 'No notes stored';
+                    list.appendChild(listItem); 
+                }
+                console.log('Notes all displayed'); 
+            }
+        };
+    }
+
+    
+}
