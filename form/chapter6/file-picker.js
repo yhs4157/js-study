174d52d5b716f3baf0e51d9a0bc4ae65956cbb27
yunhosung <yhs4157@naver.com>
2021-12-28 15:13:26 +0900

    clear Advanced form styling

---
+++ form/chapter6/file-picker.js
@@ -0,0 +1,31 @@
+const fileInput = document.querySelector("#file"); 
+const fileList = document.querySelector("#file-list"); 
+
+fileInput.addEventListener('change', updateFileList); 
+
+function updateFileList() {
+    while(fileList.firstChild) {
+        fileList.removeChild(fileList.firstChild); 
+    }
+
+    let curFiles = fileInput.files; 
+
+    if(!(curFiles.length === 0)) {
+        console.log('test'); 
+        for(let i = 0; i < curFiles.length; i++) {
+            const listItem = document.createElement('li'); 
+            listItem.textContent = `File name ${curFiles[i].name}: file size ${returnFileSize(curFiles[i].size)}`;
+            fileList.appendChild(listItem); 
+        }
+    }
+}
+
+function returnFileSize(number) {
+    if(number < 1024) {
+        return `${number} bytes`;
+    } else if(number >=1024 && number < 1024 * 1024) {
+        return `${(number/1024).toFixed(1)} KB`;
+    } else if(number >= 1024 * 1024) {
+        return `${(number/1024 * 1024).toFixed(1)} MB`;
+    }
+}
